64c9031034e6846b3c408565a8a97597be11773b
DevZarah <mojeedomowunmi3@gmail.com>
2021-12-01 11:26:16 +0100

    Guessinggame

--- Guessinggame.py
+++ Guessinggame.py
@@ -1,10 +1,11 @@
 import random
-user_input=int(input("Enter a number;"))
-random_number=random.randint(1,7)
-if user_input== random_number:
-    print(f"Your guess is {user_input} and the random number is {random_number}")
-    print("Welldone")
+for i in range(10):
+    user_input=int(input("Enter a number;"))
+    random_number=random.randint(1,10)
+    if user_input== random_number:
+        print(f"Your guess is {user_input} and the random number is {random_number}")
+        print("Welldone")
 
-else:
-    print(f"Your guess is {user_input} and the  random number is {random_number}")
-    print("Try again")
+    else:
+        print(f"Your guess is {user_input} and the  random number is {random_number}")
+        print("Try again") 
